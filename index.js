aca06d1160d47b003bdf0ba33c3a91b0796099ba
Fatema Afrin Ritu <afrinritu17@gmail.com>
2023-05-04 13:12:05 +0600

    server setup

--- index.js
+++ index.js
@@ -3,11 +3,32 @@ const app = express();
 const cors = require('cors');
 const port = process.env.PORT || 5000;
 
+
+const chefCard = require('./data/chefCard.json');
+const chefDetail = require('./data/chefDetails.json');
+
+app.use(cors());
+
 app.get('/', (req, res) => {
     res.send('The Levantine Chef Wizard is running')
 });
 
+app.get('/chefCard', (req, res) => {
+    res.send(chefCard)
+});
 
+app.get('/chefDetail', (req, res) => {
+    res.send(chefDetail)
+});
+
+// id wise data load
+app.get('/chefDetail/:id', (req, res)=>{
+    const id = parseInt(req.params.id);
+    console.log(id)
+    const selectedChefDetail = chefDetail.find(detail=> parseInt(detail.chefId) === id);
+    console.log(selectedChefDetail);
+    res.send(selectedChefDetail);
+})
 
 app.listen(port, () => {
     console.log(`Levantine Chef Wizard is running on port: ${port}`)
